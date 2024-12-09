7016e83f10c3614eda4892f99b022286fee11821
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-12-09 23:16:40 +0700

    feat: fix triggers and add dummy data

--- db/migrations/0005_add_trigger_validate_voucher_usage.sql
+++ db/migrations/0005_add_trigger_validate_voucher_usage.sql
@@ -1,36 +1,31 @@
 CREATE OR REPLACE FUNCTION validate_voucher_usage()
 RETURNS TRIGGER AS $$
 DECLARE
-    telah_digunakan INT;
-    kuota_penggunaan INT;
-    tgl_expired DATE;
+    banyak_digunakan INT;
+    kuota INT;
+    expired_tgl DATE;
 BEGIN
     -- Ambil data kuota penggunaan dan tanggal expired voucher
     SELECT NEW.telah_digunakan, kuota_penggunaan, NEW.tgl_awal + jml_hari_berlaku
-    INTO telah_digunakan, kuota_penggunaan, tgl_expired
+    INTO banyak_digunakan, kuota, expired_tgl
     FROM SIJARTA.VOUCHER
     WHERE kode = NEW.id_voucher;
 
     -- Periksa apakah kuota penggunaan voucher telah habis
-    IF telah_digunakan IS NOT NULL AND telah_digunakan > kuota_penggunaan THEN
+    IF banyak_digunakan IS NOT NULL AND banyak_digunakan > kuota THEN
         RAISE EXCEPTION 'Voucher % sudah mencapai batas jumlah penggunaan', NEW.id_voucher;
     END IF;
 
     -- Periksa apakah voucher telah melewati batas hari berlaku
-    IF tgl_expired IS NOT NULL AND tgl_expired < CURRENT_DATE THEN
+    IF expired_tgl IS NOT NULL AND expired_tgl < CURRENT_DATE THEN
         RAISE EXCEPTION 'Voucher % sudah melewati batas hari berlaku', NEW.id_voucher;
     END IF;
-
-    -- Jika semua validasi lolos, kurangi kuota penggunaan voucher
-    UPDATE SIJARTA.TR_PEMBELIAN_VOUCHER
-    SET telah_digunakan = telah_digunakan + 1
-    WHERE kode = NEW.id_voucher;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE TRIGGER validate_voucher_trigger
-BEFORE INSERT OR UPDATE ON SIJARTA.TR_PEMBELIAN_VOUCHER
+BEFORE UPDATE ON SIJARTA.TR_PEMBELIAN_VOUCHER
 FOR EACH ROW
 EXECUTE FUNCTION validate_voucher_usage();
--- db/migrations/0006_add_trigger_pengembalian_saldo_mypay_on_delete.sql
+++ db/migrations/0006_add_trigger_pengembalian_saldo_mypay_on_delete.sql
@@ -2,23 +2,24 @@ CREATE OR REPLACE FUNCTION pengembalian_saldo_mypay_on_delete()
 RETURNS TRIGGER AS $$
 DECLARE
     nominal_biaya DECIMAL(15, 2);
-    id_pelanggan UUID;
+    id_pelanggan_login UUID;
 BEGIN
     SELECT total_biaya, id_pelanggan
-    INTO nominal_biaya, id_pelanggan
+    INTO nominal_biaya, id_pelanggan_login
     FROM SIJARTA.TR_PEMESANAN_JASA
     WHERE id = OLD.id;
 
     UPDATE SIJARTA.USER
     SET saldo_mypay = saldo_mypay + nominal_biaya
-    WHERE id = id_pelanggan;
+    WHERE id = id_pelanggan_login;
 
     RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER pengembalian_saldo_mypay_trigger_delete
-AFTER DELETE
+CREATE OR REPLACE TRIGGER pengembalian_saldo_mypay_trigger_delete
+BEFORE DELETE
 ON SIJARTA.TR_PEMESANAN_JASA
 FOR EACH ROW
 EXECUTE FUNCTION pengembalian_saldo_mypay_on_delete();
+
