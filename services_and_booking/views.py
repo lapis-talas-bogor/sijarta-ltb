1378f6fdfc0a41bfa264d1175db68d3c8844ac7f
zufarra <zufarromli@gmail.com>
2024-12-09 22:10:56 +0700

    testing

--- services_and_booking/views.py
+++ services_and_booking/views.py
@@ -220,28 +220,11 @@ def create_order(request):
         message = None
         if diskon:
             valid_diskon = OrderService.check_discount_code(diskon,pelanggan_id)
-            if not valid_diskon or float(valid_diskon['min_tr_pemesanan']) > session_price:
-                messages.error(request, "Kode diskon yang Anda masukkan tidak valid atau tidak memenuhi syarat minimal transaksi.")
+            if not valid_diskon:
+                message = "Kode diskon yang Anda masukkan tidak valid."
                 return redirect("service:show_subkategori", subcategory_id=subcategory_id)
             else:
-                if valid_diskon['tipe_diskon'] == "VOUCHER":
-                    try:
-                        OrderService.mark_voucher_as_used(valid_diskon['kode'], pelanggan_id)
-                    except:
-                        messages.error(request, "Voucher telah melewati batas jumlah penggunaan atau batasan hari berlaku.")
-                        return redirect("service:show_subkategori", subcategory_id=subcategory_id)
-                else:
-                    valid_promo = OrderService.check_promo_again(valid_diskon['kode'])
-                    if not valid_promo:
-                        messages.error(request, "Promo sudah melewati tanggal akhir berlaku")
-                        return redirect("service:show_subkategori", subcategory_id=subcategory_id)
-                    
-
                 session_price -= float(valid_diskon['potongan'])
-        
-        if metode_bayar == "Pilih Metode Pembayaran...":
-            messages.error(request, "Pilih metode pembayaran anda")
-            return redirect("service:show_subkategori", subcategory_id=subcategory_id)
         
         OrderService.create_order(order_id, 
                                 tanggal_pemesanan, 
@@ -322,7 +305,7 @@ def create_testimoni(request):
 
         # Jika belum ada, lanjutkan untuk membuat testimoni baru
         TestimoniService.create_testimoni(id_tr_pemesanan, komentar, rating)
-
+        
         return redirect('service:show_booking_view')
 
 def delete_testimoni(request, testimoni_id, id_subkategori):
